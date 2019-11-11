567eb274f40fd96e37b6811eb98e56df027986e9
Victor <victorplanchuelo@gmail.com>
2019-11-11 13:11:23 +0100

    Creado el esqueleto del front

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,30 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>{{ config('app.name', 'Laravel') }}</title>
+<link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great" rel="stylesheet">
+
+<link href=" {{ asset('css/app.css') }}" rel="stylesheet">
+
+<link rel="stylesheet" href="{{ asset('css/jquery-ui.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/jquery.timepicker.min.css') }}">
+
+<link rel="stylesheet" href="{{ asset('css/open-iconic-bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+
+<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
+
+<link rel="stylesheet" href="{{ asset('css/aos.css') }}">
+
+<link rel="stylesheet" href="{{ asset('css/ionicons.min.css') }}">
+
+<link rel="stylesheet" href="{{ asset('css/flaticon.css') }}">
+<link rel="stylesheet" href="{{ asset('css/icomoon.css') }}">
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco_navbar ftco-navbar-light" id="ftco-navbar">
+    <div class="container d-flex align-items-center">
+        <a href="#" class="navbar-brand">ETERNO RECUERDO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="oi oi-menu"></span> Menu
+        </button>
+        <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item" tag="li"><a href="#" class="nav-link pl-0">Home</a></li>
+                <li class="nav-item" tag="li"><a href="#" class="nav-link">About</a></li>
+                <li class="nav-item"><a href="teacher.html" class="nav-link">Teacher</a></li>
+                <li class="nav-item"><a href="courses.html" class="nav-link">Courses</a></li>
+                <li class="nav-item"><a href="pricing.html" class="nav-link">Pricing</a></li>
+                <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+                <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
