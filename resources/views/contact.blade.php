f8f9fead26ef44835db15a41ce3e3179e14337d0
Victor <victorplanchuelo@gmail.com>
2019-11-13 11:38:22 +0100

    Añadidos componentes página Contacts

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+@include('partials/contact/breadcrumb')
+@include('partials/contact/info')
+@include('partials/contact/form')
+@endsection
---
+++ resources/views/partials/contact/map.blade.php
@@ -0,0 +1,3 @@
+<div class="col-md-6 d-flex align-items-stretch">
+    <iframe src="https://www.google.com/maps/d/embed?mid=1JFFG_pwIN-2trwAkRYNqTe05dlBOFDdy" width="640" height="480"></iframe>
+</div>
