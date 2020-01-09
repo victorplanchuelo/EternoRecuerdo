849cb9f5718b5acb422d88479431711c75e8a27f
Victor <victorplanchuelo@gmail.com>
2020-01-09 16:41:24 +0100

    Cambios en textos del carrusel

--- resources/views/partials/home/carrusel.blade.php
+++ resources/views/partials/home/carrusel.blade.php
@@ -4,7 +4,14 @@
         <div class="container">
             <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
                 <div class="col-md-8 text-center ftco-animate">
-                    <h1 class="mb-4">Eterno Recuerdo</h1>
+                    <h1 class="mb-4">
+                        <span style="font-size: 80px">DESCUENTO </span>
+                        <span style="font-size: 15px">en ramos y velas virtuales</span>
+                        <span style="font-size: 100px;">50%</span>
+                        <span style="line-height: 8"> </span>
+                        <span style="font-size: 45px;">Menciona y comparte</span>
+                        <span style="font-size: 45px;">en Redes Sociales</span>
+                </h1>
                 </div>
             </div>
         </div>
@@ -15,7 +22,7 @@
         <div class="container">
             <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
                 <div class="col-md-8 text-center ftco-animate">
-                    <h1 class="mb-4">Donde los recuerdos<span> nunca desaparecen</span></h1>
+                    <h1 class="mb-4">Recordarte siempre,<span> olvidarte jamás</span></h1>
                 </div>
             </div>
         </div>
