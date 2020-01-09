8464e094e73a6210b44bea62cbb26ed910fc6b2d
Victor <victorplanchuelo@gmail.com>
2020-01-09 17:23:53 +0100

    Cambio de fuente en el texto del carrusel

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -8,6 +8,7 @@
 <title>{{ config('app.name', 'Laravel') }}</title>
 <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Handlee&display=swap" rel="stylesheet">
 
 <link href=" {{ asset('css/app.css') }}" rel="stylesheet">
 
