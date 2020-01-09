88502c94b3fc566642ce6cd9845bd6206d87cf76
Victor <victorplanchuelo@gmail.com>
2020-01-09 16:31:42 +0100

    Cambios en carrusel debido a la segunda imagen

--- resources/views/partials/home/carrusel.blade.php
+++ resources/views/partials/home/carrusel.blade.php
@@ -4,8 +4,7 @@
         <div class="container">
             <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
                 <div class="col-md-8 text-center ftco-animate">
-                    <h1 class="mb-4">Kids Are The Best <span>Explorers In The World</span></h1>
-                    <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">Read More</a></p>
+                    <h1 class="mb-4">Eterno Recuerdo</h1>
                 </div>
             </div>
         </div>
@@ -16,8 +15,7 @@
         <div class="container">
             <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
                 <div class="col-md-8 text-center ftco-animate">
-                    <h1 class="mb-4">Perfect Learned<span> For Your Child</span></h1>
-                    <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">Read More</a></p>
+                    <h1 class="mb-4">Donde los recuerdos<span> nunca desaparecen</span></h1>
                 </div>
             </div>
         </div>
