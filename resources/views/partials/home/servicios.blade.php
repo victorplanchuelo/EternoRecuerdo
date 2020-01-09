b925192677db7bf2cad73d8661dd8db94bdf7ef6
Victor <victorplanchuelo@gmail.com>
2020-01-09 12:29:07 +0100

    Cambios en el home

--- resources/views/partials/home/servicios.blade.php
+++ resources/views/partials/home/servicios.blade.php
@@ -4,10 +4,12 @@
             <div class="col-md-3 d-flex services align-self-stretch pb-4 px-4 ftco-animate bg-primary">
                 <div class="media block-6 d-block text-center">
                     <div class="icon d-flex justify-content-center align-items-center">
-                        <span class="flaticon-teacher"></span>
+                        <span>
+                            <img class="img-servicios-home" src="{{asset('images/servicios/ser-querido-manos.png')}}" />
+                        </span>
                     </div>
                     <div class="media-body p-2 mt-3">
-                        <h3 class="heading">Certified Teachers</h3>
+                        <h3 class="heading">Ser querido</h3>
                         <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
                     </div>
                 </div>
@@ -15,10 +17,12 @@
             <div class="col-md-3 d-flex services align-self-stretch pb-4 px-4 ftco-animate bg-tertiary">
                 <div class="media block-6 d-block text-center">
                     <div class="icon d-flex justify-content-center align-items-center">
-                        <span class="flaticon-reading"></span>
+                        <span>
+                            <img class="img-servicios-home" src="{{asset('images/servicios/mascota-manos.png')}}" />
+                        </span>
                     </div>
                     <div class="media-body p-2 mt-3">
-                        <h3 class="heading">Special Education</h3>
+                        <h3 class="heading">Mascota</h3>
                         <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
                     </div>
                 </div>
@@ -26,10 +30,12 @@
             <div class="col-md-3 d-flex services align-self-stretch pb-4 px-4 ftco-animate bg-fifth">
                 <div class="media block-6 d-block text-center">
                     <div class="icon d-flex justify-content-center align-items-center">
-                        <span class="flaticon-books"></span>
+                        <span>
+                            <img class="img-servicios-home" src="{{asset('images/servicios/servicios-manos-2.png')}}" />
+                        </span>
                     </div>
                     <div class="media-body p-2 mt-3">
-                        <h3 class="heading">Book &amp; Library</h3>
+                        <h3 class="heading">Nuestros servicios</h3>
                         <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
                     </div>
                 </div>
@@ -37,10 +43,12 @@
             <div class="col-md-3 d-flex services align-self-stretch pb-4 px-4 ftco-animate bg-quarternary">
                 <div class="media block-6 d-block text-center">
                     <div class="icon d-flex justify-content-center align-items-center">
-                        <span class="flaticon-diploma"></span>
+                        <span>
+                            <img class="img-servicios-home" src="{{asset('images/servicios/ayuda-manos.png')}}" />
+                        </span>
                     </div>
                     <div class="media-body p-2 mt-3">
-                        <h3 class="heading">Certification</h3>
+                        <h3 class="heading">Tutoriales y ayuda</h3>
                         <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
                     </div>
                 </div>
