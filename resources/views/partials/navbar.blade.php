8309038222cd46cf093c2ef4f930b3f8cb1a1863
Victor <victorplanchuelo@gmail.com>
2020-01-09 14:08:15 +0100

    Añadido logo en el navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco_navbar ftco-navbar-light" id="ftco-navbar">
     <div class="container d-flex align-items-center">
-        <a href="{{url('/')}}" class="navbar-brand">ETERNO RECUERDO</a>
+        <a href="{{url('/')}}" class="navbar-brand">
+            <img style="width:20%" src="{{asset('images/logo.png')}}" />
+            <span class="col-md-1"> </span>
+            ETERNO RECUERDO
+        </a>
+        <!--<a href="{{url('/')}}" class="navbar-brand">ETERNO RECUERDO</a>-->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="oi oi-menu"></span> Menu
         </button>
