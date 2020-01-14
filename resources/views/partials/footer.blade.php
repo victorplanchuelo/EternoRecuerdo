9684d9bcf6b3f5b68b5480370424b3e424d22230
Victor <victorplanchuelo@gmail.com>
2020-01-14 14:01:08 +0100

    Corrección del footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,19 +3,19 @@
         <div class="row mb-5">
             <div class="col-md-6 col-lg-3">
                 <div class="ftco-footer-widget mb-5">
-                    <h2 class="ftco-heading-2">Have a Questions?</h2>
+                    <h2 class="ftco-heading-2">Contacto</h2>
                     <div class="block-23 mb-3">
                         <ul>
-                            <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-                            <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+                            <li><span class="icon icon-map-marker"></span><span class="text">Carrer Almirante Antequera, 9. 03130. Santa Pola (Alicante)</span></li>
+                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">+34 123 45 67 89</span></a></li>
+                            <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@eternorecuerdo.com</span></a></li>
                         </ul>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 col-lg-3">
                 <div class="ftco-footer-widget mb-5">
-                    <h2 class="ftco-heading-2">Recent Blog</h2>
+                    <h2 class="ftco-heading-2">Últimos homenajes</h2>
                     <div class="block-21 mb-4 d-flex">
                         <a class="blog-img mr-4" style="background-image: url('/images/image_1.jpg');"></a>
                         <div class="text">
@@ -42,26 +42,17 @@
             </div>
             <div class="col-md-6 col-lg-3">
                 <div class="ftco-footer-widget mb-5 ml-md-4">
-                    <h2 class="ftco-heading-2">Links</h2>
+                    <h2 class="ftco-heading-2">Enlaces</h2>
                     <ul class="list-unstyled">
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>About</a></li>
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Services</a></li>
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Deparments</a></li>
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Contact</a></li>
+                        <li><a href="{{url('/')}}"><span class="ion-ios-arrow-round-forward mr-2"></span>Inicio</a></li>
+                        <li><a href="{{url('about')}}"><span class="ion-ios-arrow-round-forward mr-2"></span>Sobre nosotros</a></li>
+                        <li><a href="{{url('homenajes')}}"><span class="ion-ios-arrow-round-forward mr-2"></span>Homenajes</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Colaboradores</a></li>
+                        <li><a href="{{url('contacto')}}"><span class="ion-ios-arrow-round-forward mr-2"></span>Contacto</a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-md-6 col-lg-3">
-                <div class="ftco-footer-widget mb-5">
-                    <h2 class="ftco-heading-2">Subscribe Us!</h2>
-                    <form action="#" class="subscribe-form">
-                        <div class="form-group">
-                            <input type="text" class="form-control mb-2 text-center" placeholder="Enter email address">
-                            <input type="submit" value="Subscribe" class="form-control submit px-3">
-                        </div>
-                    </form>
-                </div>
                 <div class="ftco-footer-widget mb-5">
                     <h2 class="ftco-heading-2 mb-0">Connect With Us</h2>
                     <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
