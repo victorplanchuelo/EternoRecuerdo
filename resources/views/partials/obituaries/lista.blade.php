dc8acbcc68cdfd2a63595ab6666da5e976dcf8aa
Victor <victorplanchuelo@gmail.com>
2020-01-15 09:32:45 +0100

    Cambios en Homenajes

--- resources/views/partials/obituaries/lista.blade.php
+++ resources/views/partials/obituaries/lista.blade.php
@@ -3,15 +3,15 @@
         <div class="row">
             <div class="col-md-6 col-lg-4 ftco-animate">
                 <div class="blog-entry">
-                    <a href="{{route('homenajes.show',['id' => 1])}}" class="block-20 d-flex align-items-end" style="background-image: url('images/image_1.jpg');">
+                    <a href="{{route('homenajes.show',['id' => 1])}}" class="block-20 d-flex align-items-end" style="background-image: url('images/homenajes/camilo-sesto-1.jpg');">
                         <div class="meta-date text-center p-2">
-                            <span class="day">13</span>
-                            <span class="mos">Enero</span>
-                            <span class="yr">1980</span>
+                            <span class="day">16</span>
+                            <span class="mos">Septiembre</span>
+                            <span class="yr">1946</span>
                         </div>
                         <div class="meta-date text-center p-2">
-                            <span class="day">27</span>
-                            <span class="mos">Abril</span>
+                            <span class="day">08</span>
+                            <span class="mos">Septiembre</span>
                             <span class="yr">2019</span>
                         </div>
                     </a>
@@ -23,15 +23,15 @@
             </div>
             <div class="col-md-6 col-lg-4 ftco-animate">
                 <div class="blog-entry">
-                    <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/image_2.jpg');">
+                    <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/homenajes/godzilla-mascota.jpg');">
                         <div class="meta-date text-center p-2">
-                            <span class="day">13</span>
-                            <span class="mos">Enero</span>
-                            <span class="yr">1980</span>
+                            <span class="day">--</span>
+                            <span class="mos">--</span>
+                            <span class="yr">2007</span>
                         </div>
                         <div class="meta-date text-center p-2">
-                            <span class="day">27</span>
-                            <span class="mos">Abril</span>
+                            <span class="day">26</span>
+                            <span class="mos">Junio</span>
                             <span class="yr">2019</span>
                         </div>
                     </a>
@@ -43,15 +43,15 @@
             </div>
             <div class="col-md-6 col-lg-4 ftco-animate">
                 <div class="blog-entry">
-                    <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/image_3.jpg');">
+                    <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/homenajes/nanny-mascota.jpg');">
                         <div class="meta-date text-center p-2">
-                            <span class="day">13</span>
-                            <span class="mos">Enero</span>
-                            <span class="yr">1980</span>
+                            <span class="day">24</span>
+                            <span class="mos">Julio</span>
+                            <span class="yr">2009</span>
                         </div>
                         <div class="meta-date text-center p-2">
-                            <span class="day">27</span>
-                            <span class="mos">Abril</span>
+                            <span class="day">16</span>
+                            <span class="mos">Septiembre</span>
                             <span class="yr">2019</span>
                         </div>
                     </a>
@@ -63,15 +63,35 @@
             </div>
             <div class="col-md-6 col-lg-4 ftco-animate">
                 <div class="blog-entry">
-                    <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/image_4.jpg');">
+                    <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/homenajes/pilar-borbon.jpg');">
                         <div class="meta-date text-center p-2">
-                            <span class="day">13</span>
-                            <span class="mos">Enero</span>
-                            <span class="yr">1980</span>
+                            <span class="day">30</span>
+                            <span class="mos">Julio</span>
+                            <span class="yr">1936</span>
                         </div>
                         <div class="meta-date text-center p-2">
-                            <span class="day">27</span>
-                            <span class="mos">Abril</span>
+                            <span class="day">08</span>
+                            <span class="mos">Enero</span>
+                            <span class="yr">2020</span>
+                        </div>
+                    </a>
+                    <div class="text bg-white p-4">
+                        <h3 class="heading"><a href="#">Skills To Develop Your Child Memory</a></h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 ftco-animate">
+                <div class="blog-entry">
+                    <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/homenajes/punset.jpg');">
+                        <div class="meta-date text-center p-2">
+                            <span class="day">09</span>
+                            <span class="mos">Noviembre</span>
+                            <span class="yr">1936</span>
+                        </div>
+                        <div class="meta-date text-center p-2">
+                            <span class="day">22</span>
+                            <span class="mos">Mayo</span>
                             <span class="yr">2019</span>
                         </div>
                     </a>
@@ -83,36 +103,16 @@
             </div>
             <div class="col-md-6 col-lg-4 ftco-animate">
                 <div class="blog-entry">
-                    <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/image_5.jpg');">
+                    <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/homenajes/rocky-mascota.jpg');">
                         <div class="meta-date text-center p-2">
-                            <span class="day">13</span>
-                            <span class="mos">Enero</span>
-                            <span class="yr">1980</span>
-                        </div>
-                        <div class="meta-date text-center p-2">
-                            <span class="day">27</span>
+                            <span class="day">01</span>
                             <span class="mos">Abril</span>
-                            <span class="yr">2019</span>
+                            <span class="yr">2017</span>
                         </div>
-                    </a>
-                    <div class="text bg-white p-4">
-                        <h3 class="heading"><a href="#">Skills To Develop Your Child Memory</a></h3>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 ftco-animate">
-                <div class="blog-entry">
-                    <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/image_6.jpg');">
                         <div class="meta-date text-center p-2">
-                            <span class="day">13</span>
+                            <span class="day">15</span>
                             <span class="mos">Enero</span>
-                            <span class="yr">1980</span>
-                        </div>
-                        <div class="meta-date text-center p-2">
-                            <span class="day">27</span>
-                            <span class="mos">Abril</span>
-                            <span class="yr">2019</span>
+                            <span class="yr">2020</span>
                         </div>
                     </a>
                     <div class="text bg-white p-4">
