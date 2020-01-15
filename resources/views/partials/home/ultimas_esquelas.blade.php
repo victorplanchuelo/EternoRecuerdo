09d4f3f121acd2d22d445a68f0eea14f386fed8e
Victor <victorplanchuelo@gmail.com>
2020-01-15 09:15:27 +0100

    Modificación ultimos homenajes en el Home

--- resources/views/partials/home/ultimas_esquelas.blade.php
+++ resources/views/partials/home/ultimas_esquelas.blade.php
@@ -2,8 +2,7 @@
     <div class="container">
         <div class="row justify-content-center mb-5 pb-2">
             <div class="col-md-8 text-center heading-section ftco-animate">
-                <h2 class="mb-4"><span>What Parents</span> Says About Us</h2>
-                <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+                <h2 class="mb-4"><span>Últimos</span> Homenajes</h2>
             </div>
         </div>
         <div class="row ftco-animate justify-content-center">
@@ -11,71 +10,85 @@
                 <div class="carousel-testimony owl-carousel">
                     <div class="item">
                         <div class="testimony-wrap d-flex">
-                            <div class="user-img mr-4" style="background-image: url('images/teacher-1.jpg')">
+                            <div class="user-img mr-4" style="background-image: url('images/homenajes/camilo-sesto-1.jpg')">
                             </div>
                             <div class="text ml-2 bg-light">
                                 <span class="quote d-flex align-items-center justify-content-center">
                                     <i class="icon-quote-left"></i>
                                 </span>
-                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                                <p class="name">Racky Henderson</p>
-                                <span class="position">Father</span>
+                                <p>El cantante Camilo Sesto ha fallecido la madrugada del domingo 8 de septiembre a los 72 años, en Pozuelo de Alarcón, Madrid.</p>
+                                <p class="name">Camilo Blanes Cortés (Camilo Sesto)</p>
+                                <span class="position">Alcoy, 16/09/1946 - Madrid, 08/09/2019</span>
                             </div>
                         </div>
                     </div>
                     <div class="item">
                         <div class="testimony-wrap d-flex">
-                            <div class="user-img mr-4" style="background-image: url('images/teacher-2.jpg')">
+                            <div class="user-img mr-4" style="background-image: url('images/homenajes/pilar-borbon.jpg')">
                             </div>
                             <div class="text ml-2 bg-light">
                                 <span class="quote d-flex align-items-center justify-content-center">
                                     <i class="icon-quote-left"></i>
                                 </span>
-                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                                <p class="name">Henry Dee</p>
-                                <span class="position">Mother</span>
+                                <p>La Infanta Pilar de Borbón, Duquesa de Badajoz, ha fallecido en la mañana del día 8 de enero de 2010, en Madrid, a los 83 años de edad</p>
+                                <p class="name">Infanta Pilar de Borbón</p>
+                                <span class="position">Cannes, 30/07/1936 - Madrid, 08/01/2020</span>
                             </div>
                         </div>
                     </div>
                     <div class="item">
                         <div class="testimony-wrap d-flex">
-                            <div class="user-img mr-4" style="background-image: url('images/teacher-3.jpg')">
+                            <div class="user-img mr-4" style="background-image: url('images/homenajes/punset.jpg')">
                             </div>
                             <div class="text ml-2 bg-light">
                                 <span class="quote d-flex align-items-center justify-content-center">
                                     <i class="icon-quote-left"></i>
                                 </span>
-                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                                <p class="name">Mark Huff</p>
-                                <span class="position">Mother</span>
+                                <p>Eduard Punset ha fallecido en Barcelona, el día 22 de mayo de 2019, a las 82 años de edad. Gracias por acercar la ciencia a las personas</p>
+                                <p class="name">Eduard Punset Casals</p>
+                                <span class="position">Barcelona, 09/11/1936 - Barcelona, 22/05/2019</span>
                             </div>
                         </div>
                     </div>
                     <div class="item">
                         <div class="testimony-wrap d-flex">
-                            <div class="user-img mr-4" style="background-image: url('images/teacher-4.jpg')">
+                            <div class="user-img mr-4" style="background-image: url('images/homenajes/rocky-mascota.jpg')">
                             </div>
                             <div class="text ml-2 bg-light">
                                 <span class="quote d-flex align-items-center justify-content-center">
                                     <i class="icon-quote-left"></i>
                                 </span>
-                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                                <p class="name">Rodel Golez</p>
-                                <span class="position">Mother</span>
+                                <p>Rocky falleció en Blanes el día 15 de Enero de 2020 a la edad de 3 años. Tu familia y amigos no te olvidan</p>
+                                <p class="name">Rocky</p>
+                                <span class="position">Ribadesella, 01/04/2017 - Blanes, 15/01/2020</span>
                             </div>
                         </div>
                     </div>
                     <div class="item">
                         <div class="testimony-wrap d-flex">
-                            <div class="user-img mr-4" style="background-image: url('images/teacher-1.jpg')">
+                            <div class="user-img mr-4" style="background-image: url('images/homenajes/nanny-mascota.jpg')">
                             </div>
                             <div class="text ml-2 bg-light">
                                 <span class="quote d-flex align-items-center justify-content-center">
                                     <i class="icon-quote-left"></i>
                                 </span>
-                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                                <p class="name">Ken Bosh</p>
-                                <span class="position">Mother</span>
+                                <p>Gracias Nanny por tu compañía. Nunca podremos olvidar el cariño que nos dabas. Que la tierra te sea leve, amiga mía</p>
+                                <p class="name">Nanny</p>
+                                <span class="position">Madrid, 24/07/2009 - Madrid, 16/09/2019</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="testimony-wrap d-flex">
+                            <div class="user-img mr-4" style="background-image: url('images/homenajes/godzilla-mascota.jpg')">
+                            </div>
+                            <div class="text ml-2 bg-light">
+                                <span class="quote d-flex align-items-center justify-content-center">
+                                    <i class="icon-quote-left"></i>
+                                </span>
+                                <p>Fue una tortuga muy feliz, comilona y un fiel compañero. Después de tanto tiempo ahora descansa en paz. </p>
+                                <p class="name">Godzilla</p>
+                                <span class="position">Alicante, 2007 - Ibi, 26/06/2019</span>
                             </div>
                         </div>
                     </div>
