21348f81a960c3f9fe24c24015de899afa6e200c
Victor <victorplanchuelo@gmail.com>
2020-01-16 08:50:41 +0100

    Corrección en el buscador del Home

--- resources/views/partials/home/buscador.blade.php
+++ resources/views/partials/home/buscador.blade.php
@@ -5,8 +5,8 @@
             <div class="col-md-12">
                 <h2 class="text-center">Busca a tu ser querido o mascota</h2>
 
-                <form action="#" class="search-form form-inline">
-                    <div class="form-group col-md-12">
+                <form action="#" class="search-form form-inline justify-content-center">
+                    <div class="form-group col-md-8">
                     
                         <input type="text" style="border-radius: 25px;" class="form-control col-md-8 ml-5" placeholder="Nombre a buscar">
                         <button class="btn btn-tertiary ml-3 col-md-2">Buscar</button>
