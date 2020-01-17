d9065a24296b7f1a741333ec5bdbc34aeb35468b
Victor <victorplanchuelo@gmail.com>
2020-01-17 12:04:17 +0100

    Cambios en página Sobre Nosotros

--- resources/views/partials/about/empresa.blade.php
+++ resources/views/partials/about/empresa.blade.php
@@ -1,9 +1,22 @@
-<section class="ftco-section">
+<section class="ftco-section ftco-no-pb">
     <div class="container">
         <div class="row justify-content-center mb-5 pb-2">
             <div class="col-md-8 text-center heading-section ftco-animate">
                 <h2 class="mb-4"><span>La</span> Empresa</h2>
-                <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+                <p>La empresa “Eterno Recuerdo” se fundó con la ilusión de cuatro compañeras estudiantes del ciclo de administración y finanzas que buscábamos ofrecer un servicio diferente, crear un espacio íntimo y personal donde se pudiera recordar a los seres queridos y que además permitiera reunir a amigos y familiares en torno a un ser querido que ya no se encuentra con nosotros, para compartir sus recuerdos.</p>
+            </div>
+        </div>
+        <div class="row justify-content-center mb-5 pb-2">
+            <div class="col-md-8 text-center heading-section ftco-animate">
+                <h2 class="mb-4"><span>Actividad</span> de la empresa</h2>
+                <p>La principal actividad de la empresa es la de ofrecer un espacio virtual, donde los familiares que deseen pueden publicar fotos, vídeos, anécdotas e historias de sus seres queridos.</p>
+                <p>Tendrán la opción de recordarles con velas y flores virtuales, también podrán confeccionar un álbum de fotos en soporte papel.</p>
+            </div>
+        </div>
+        <div class="row justify-content-center mb-5 pb-2">
+            <div class="col-md-8 text-center heading-section ftco-animate">
+                <h2 class="mb-4"><span>Cultura</span> de la empresa</h2>
+                <p>La ética de nuestra empresa se basa en aprender cada dia tanto de los éxitos como de los fracasos, ofrecer apoyo y ayuda a todo el que lo necesite y sobre todo saber escuchar para mejorar. También estamos concienciados con el medio ambiente, para eso, realizamos acciones que ayuden a reducir las consecuencias perjudiciales para el planeta, por ello, Eterno Recuerdo es virtual.</p>
             </div>
         </div>
     </div>
--- resources/views/partials/about/trabajadoras.blade.php
+++ resources/views/partials/about/trabajadoras.blade.php
@@ -1,9 +1,8 @@
-<section class="ftco-section ftco-no-pb">
+<section class="ftco-section ftco-no-pb ftco-no-pt">
     <div class="container">
         <div class="row justify-content-center mb-5 pb-2">
             <div class="col-md-8 text-center heading-section ftco-animate">
                 <h2 class="mb-4"><span>Quienes</span> somos</h2>
-                <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
             </div>
         </div>
         <div class="row">
@@ -14,9 +13,9 @@
                     </div>
                     <div class="text pt-3 text-center">
                         <h3>Silvia Ruiz</h3>
-                        <span class="position mb-2">Teacher</span>
+                        <span class="position mb-2">RR.HH.</span>
                         <div class="faded">
-                            <p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+                            <p>Me  considero una persona con iniciativa, trabajadora, responsable y con ganas de aprender.</p>
                             <ul class="ftco-social text-center">
                                 <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
                                 <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
@@ -33,10 +32,10 @@
                         <div class="img align-self-stretch" style="background-image: url('images/teacher-2.jpg');"></div>
                     </div>
                     <div class="text pt-3 text-center">
-                        <h3>Verónica</h3>
-                        <span class="position mb-2">English Teacher</span>
+                        <h3>Verónica (Apellido)</h3>
+                        <span class="position mb-2">Comercial y Marketing</span>
                         <div class="faded">
-                            <p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+                            <p>Me considero una persona responsable, entusiasta y siempre intento dar lo mejor de mi.</p>
                             <ul class="ftco-social text-center">
                                 <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
                                 <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
@@ -53,10 +52,10 @@
                         <div class="img align-self-stretch" style="background-image: url('images/teacher-3.jpg');"></div>
                     </div>
                     <div class="text pt-3 text-center">
-                        <h3>Loreto</h3>
-                        <span class="position mb-2">Art Teacher</span>
+                        <h3>Loreto Canales</h3>
+                        <span class="position mb-2">Financiera</span>
                         <div class="faded">
-                            <p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+                            <p>Me considero una persona con muchas ganas e ilusión y este proyecto me ayudará a crecer.</p>
                             <ul class="ftco-social text-center">
                                 <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
                                 <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
@@ -73,10 +72,10 @@
                         <div class="img align-self-stretch" style="background-image: url('images/teacher-4.jpg');"></div>
                     </div>
                     <div class="text pt-3 text-center">
-                        <h3>Mª del Pilar</h3>
-                        <span class="position mb-2">Science Teacher</span>
+                        <h3>Mª del Pilar (Apellido)</h3>
+                        <span class="position mb-2">Contabilidad</span>
                         <div class="faded">
-                            <p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+                            <p>Me considero una persona que se adapta muy bien a los cambios, y me gusta dar lo mejor de mí en todo momento y ayudar a los demás.</p>
                             <ul class="ftco-social text-center">
                                 <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
                                 <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
