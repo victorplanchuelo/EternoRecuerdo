f4b5e35e5b26d923580436822e5d837a85894fd0
Victor <victorplanchuelo@gmail.com>
2020-02-12 13:22:06 +0100

    Añadido ejemplo Cards en Servicios

--- resources/views/partials/services/velas.blade.php
+++ resources/views/partials/services/velas.blade.php
@@ -1,33 +1,67 @@
 <section class="ftco-gallery velas-section" style="width:100%;">
   <div class="container">
     <div class="row">
-      <div class="col-md-3 ftco-animate">
-        <a href="/images/homenajes/velas/modelo1/4.png" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/homenajes/velas/modelo1/4.png);">
-          <div class="icon mb-4 d-flex align-items-center justify-content-center">
-            <span class="icon-instagram"></span>
+      <div class="card col-md-3" style="width: 18rem;">
+        <div class="ftco-animate">
+          <a href="/images/homenajes/velas/modelo1/4.png" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/homenajes/velas/modelo1/4.png);">
+            <div class="icon mb-4 d-flex align-items-center justify-content-center">
+              <span class="icon-instagram"></span>
+            </div>
+          </a>
+        </div>
+        <div class="card-body">
+          <div class="container">
+            <div class="row">
+              <div class="precio-tachado">
+                <del>
+                  <span class="amount-del">€35,00</span>
+                </del>
+                <ins>
+                  <span class="amount">€32,50</span>
+                </ins>
+              </div>
+            </div>
+            <div class="row">
+              <p class="card-text">Vela rectangular verde con logotipo.</p>
+            </div>
           </div>
-        </a>
+        </div>
       </div>
-      <div class="col-md-3 ftco-animate">
-        <a href="/images/homenajes/velas/modelo1/2.png" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/homenajes/velas/modelo1/2.png);">
-          <div class="icon mb-4 d-flex align-items-center justify-content-center">
-            <span class="icon-instagram"></span>
-          </div>
-        </a>
+      <div class="card col-md-3" style="width: 18rem;">
+        <div class="ftco-animate">
+          <a href="/images/homenajes/velas/modelo1/2.png" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/homenajes/velas/modelo1/2.png);">
+            <div class="icon mb-4 d-flex align-items-center justify-content-center">
+              <span class="icon-instagram"></span>
+            </div>
+          </a>
+        </div>
+        <div class="card-body">
+          <p class="card-text">Vela rectangular roja con logotipo.</p>
+        </div>
       </div>
-      <div class="col-md-3 ftco-animate">
-        <a href="/images/homenajes/velas/modelo2/3.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/homenajes/velas/modelo2/3.jpg);">
-          <div class="icon mb-4 d-flex align-items-center justify-content-center">
-            <span class="icon-instagram"></span>
-          </div>
-        </a>
+      <div class="card col-md-3" style="width: 18rem;">
+        <div class="ftco-animate">
+          <a href="/images/homenajes/velas/modelo2/3.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/homenajes/velas/modelo2/3.jpg);">
+            <div class="icon mb-4 d-flex align-items-center justify-content-center">
+              <span class="icon-instagram"></span>
+            </div>
+          </a>
+        </div>
+        <div class="card-body">
+          <p class="card-text">Vela esférica azul con logotipo.</p>
+        </div>
       </div>
-      <div class="col-md-3 ftco-animate">
-        <a href="/images/homenajes/velas/modelo2/1.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/homenajes/velas/modelo2/1.jpg);">
-          <div class="icon mb-4 d-flex align-items-center justify-content-center">
-            <span class="icon-instagram"></span>
-          </div>
-        </a>
+      <div class="card col-md-3" style="width: 18rem;">
+        <div class="ftco-animate">
+          <a href="/images/homenajes/velas/modelo2/1.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/homenajes/velas/modelo2/1.jpg);">
+            <div class="icon mb-4 d-flex align-items-center justify-content-center">
+              <span class="icon-instagram"></span>
+            </div>
+          </a>
+        </div>
+        <div class="card-body">
+          <p class="card-text">Vela esférica verde con logotipo.</p>
+        </div>
       </div>
     </div>
   </div>
