87442760acc2f11ddf57b610ee621e0cd98b4758
Victor <victorplanchuelo@gmail.com>
2020-02-18 17:00:47 +0100

    Añadido catálogo al servicio

--- resources/views/partials/services/services.blade.php
+++ resources/views/partials/services/services.blade.php
@@ -1,19 +1,28 @@
 <section class="ftco-section">
     <div class="container">
         <div class="row">
-            <h1>Velas</h1>
+            <h1 class="texto-servicios mx-auto font-weight-bold">Velas</h1>
         </div>
         <div class="row">
             @include('partials/services/velas')
         </div>
         <div class="row">
-            <h1>Ramos</h1>
+            <h1  class="texto-servicios mx-auto font-weight-bold">Ramos</h1>
         </div>
         <div class="row">
             @include('partials/services/ramos')
         </div>
         <div class="row mt-5">
-            <h1>Descuentos en la página</h1>
+            <h1  class="texto-servicios mx-auto font-weight-bold">También puede echar un vistazo a nuestro folleto</h1>
+        </div>
+        <div class="row">
+            <object data="/images/catalogo.pdf" type="application/pdf" width="100%" height="500px"> 
+                <p>Parece que no tienes instalado el plugin para PDF en tu navegador.
+                No te preocupes, puedes <a href="/images/catalogo.pdf">pinchar aquí para descargarte el documento.</a></p>  
+            </object>
+        </div>
+        <div class="row mt-5">
+            <h1  class="texto-servicios mx-auto font-weight-bold">Descuentos en la página</h1>
         </div>
         <div class="row">
             <div class="col-4 mx-auto text-center">
